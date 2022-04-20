95d78125f2b2d464779b7f633560d3cbda3f0c6e
Luian Ortiz <luiann.ortiz@gmail.com>
2022-04-20 11:45:27 -0500

    Sprint 2, 3th requirement: Update Username

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,20 +1,30 @@
 const User = require('./../models/User')
 
 class UserService{
+
+    /**
+     * Creacion de un usuario mediante UserService
+     * 
+     */
     static create(id, username, name){
         return new User(id, username, name, "Sin bio")
     }
+
+     /**
+     * Convertir el objeto user a una lista de atributos
+     */
     static getInfo(user){
         return Object.values(user)
     }
+
+    /**
+     * Cambiar el nombre de usuario
+     * 
+     */
     static updateUserUsername(user, newUserName){
-        user = new User()
-        user.setUsername = newUserName
+        user.setUsername = newUserName;
         return user
     }
 }
-
-const update = UserService.updateUserUsername()
-console.log(update)
 
 module.exports = UserService
